1ab2289c400f6ef3ff6e4df430762b6a1b5da3d9
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-18 10:44:21 -0700

    add basic structure to Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="home">
+    <nav class="sticky">
+      <div class="nav-content">
+        <h3>S&J</h3>
+        <img src="../img/nav-hamburger.png" class="ham-button">
+        <img src="../img/nav-hamburger-close.png" class="ham-button-close">
+      </div>
+      <div class="main-nav">
+        <a href="index.html" class="active">Home</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+        <a href="#">Projects</a>
+      </div>
+    </nav>
+
+    <div class="home container">
+      <div class="jumbotron">
+        <img src="../img/home/home-jumbotron.png" alt="picture of one of our works" class="d-image">
+        <img src="../img/home/home-mobile-jumbotron.png" alt="picture of one of our works" class="m-image">
+        <div class="jumbotron-text">
+          <h1>
+            Integrity,<br>
+            Excellence,<br>
+            Progress.<br>
+          </h1>
+        </div>
+      </div>
+      <div class="main-content">
+        <div class="content-box m-image">
+          <img src="../img/home/home-mobile-img-1.png" alt="image of pen and paper" class="m-image">
+        </div>
+        <div class="content-box content-box-text content-box-first os-animation" data-os-animation="fadeInLeft" data-os-animation-delay="0s">
+          <h3>Smith & Jones Architects</h3>
+          <p>Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer sed. Duo etiam laboramus dissentiet in, nec no errem </p>
+          <button>Learn More</button>
+        </div>
+        <div class="content-box os-animation" data-os-animation="fadeInRight" data-os-animation-delay="0s">
+          <img src="../img/home/home-img-1.png" alt="image of pen and paper" class="d-image">
+        </div>
+        <div class="content-box os-animation" data-os-animation="fadeInLeft" data-os-animation-delay="0s">
+          <img src="../img/home/home-img-2.png" alt="image of futuristic design" class="d-image">
+          <img src="../img/home/home-mobile-img-2.png" alt="image of futuristic design" class="m-image">
+        </div>
+        <div class="content-box content-box-text content-box-last os-animation" data-os-animation="fadeInRight" data-os-animation-delay="0s">
+          <h3>Futuristic Designs</h3>
+          <p>Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer sed. Duo etiam laboramus dissentiet in, nec no errem</p>
+          <button>View Designs</button>
+        </div>
+        <div class="recent-projects">
+          <h3>Recent Projects</h3>
+          <div class="project">
+            <div class="main-image os-animation" data-os-animation="fadeInRight" data-os-animation-delay="0s">
+              <img src="../img/home/home-villas-img.png" alt="pictures of villas" class="d-image">
+              <img src="../img/home/home-mobile-villas-img.png" alt="pictures of villas" class="m-image">
+              <div class="title">
+                <h3>THE VILLAS</h3>
+              </div>
+            </div>
+            <div class="project-content os-animation" data-os-animation="pulse" data-os-animation-delay="0s">
+              <p>The Villas bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. </p>
+              <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+            </div>
+          </div>
+          <div class="project">
+            <div class="main-image os-animation" data-os-animation="fadeInLeft" data-os-animation-delay="0s">
+              <img src="../img/home/home-outskirts-img.png" alt="pictures of outskirts" class="d-image">
+              <img src="../img/home/home-mobile-outskirts-img.png" alt="pictures of outskirts" class="m-image">
+              <div class="title left-title">
+                <h3>OUTSKIRTS</h3>
+              </div>
+            </div>
+            <div class="project-content os-animation" data-os-animation="pulse" data-os-animation-delay="0s">
+              <p>The Outskirts are amazing to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. </p>
+              <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+            </div>
+          </div>
+          <div class="project">
+            <div class="main-image os-animation" data-os-animation="fadeInRight" data-os-animation-delay="0s">
+              <img src="../img/home/home-the-blocks-img.png" alt="pictures of blocks" class="d-image">
+              <img src="../img/home/home-mobile-the-blocks-img.png" alt="pictures of blocks" class="m-image">
+              <div class="title">
+                <h3>THE BLOCKS</h3>
+              </div>
+            </div>
+            <div class="project-content os-animation" data-os-animation="pulse" data-os-animation-delay="0s">
+              <p>The Blocks are amazing to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. </p>
+              <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer class="container">
+      <div class="footer-content">
+        <div class="email">
+          <h3>Interested in starting a project?<br>Let’s talk:</h3>
+          <input type="text" name="email" placeholder="Enter email">
+          <h5>We'll never share your email with anyone else.</h5>
+        </div>
+        <div class="location">
+          <h3>New York</h3>
+          <p>123 Lane<br>
+          Suite 100<br>
+          Albany, NY 12345<br>
+          202 555 0144
+          </p>
+        </div>
+        <div class="location">
+          <h3>Florida</h3>
+          <p>Ocean Drive<br>
+            Suite 201<br>
+            Orlando, FL 22345<br>
+            502 555 0144
+          </p>
+        </div>
+        <div class="location">
+          <h3>California</h3>
+          <p>Mountain Street<br>
+            Suite 105<br>
+            San Diego, CA 22345<br>
+            702 555 0144
+          </p>
+        </div>
+        <div class="copyright">
+          <h5>Copyright © 2018 Smith and Jones</h5>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
